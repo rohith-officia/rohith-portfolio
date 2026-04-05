dfe9161202bed6dd9b5be71ee90c6b777ac483f4
rohith <rohith.officia@gmail.com>
2026-04-05 19:26:34 +0530

    Fix build for Vercel2

--- src/React/Project.jsx
+++ src/React/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./css/projects.css";
 
