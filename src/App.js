6312ce87bbf4a10b4d718980a27f26e28247f573
rohith <rohith.officia@gmail.com>
2026-04-08 23:27:59 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ function App() {
   return (
     <>
       <Navbar />
+     <div className="page-content">
       <Hero />
       <About />
       <Experience />
       <Project />
       <Proficiency />
       <Services />
+    </div>
   
     </>
   );
